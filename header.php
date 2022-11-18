cc2aaadb3d1eb7bbf953425bc44d04e2a9bd8bb0
grupo ccima <ccimagithub@gmail.com>
2022-11-18 13:46:38 -0600

    ccima22

--- header.php
+++ header.php
@@ -1,15 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./public/bootstrap5/bootstrap.min.css">
     <link rel="stylesheet" href="./public/fontawesome-free-6.2.0-web/css/all.css">
     <link rel="stylesheet" href="./public/fontawesome-free-6.2.0-web/css/all.min.css">
     <link rel="stylesheet" href="./public/css/estilo.css">
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.1/css/jquery.dataTables.css">
-
+    <link href="https://unpkg.com/vanilla-datatables@latest/dist/vanilla-dataTables.min.css" rel="stylesheet" type="text/css">
 
     <title>Crud</title>
 </head>
