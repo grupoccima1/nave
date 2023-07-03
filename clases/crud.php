094fd29c5d4ec161e27fea02f359cb0cc2ade3cd
grupoccima1 <111792599+grupoccima1@users.noreply.github.com>
2023-07-03 14:26:01 -0600

    Add files via upload

--- clases/crud.php
+++ clases/crud.php
@@ -4,7 +4,7 @@ class Crud extends Conexion {
     public function mostrarDatos(){
         try {
             $conexion = parent::conectar();
-            $coleccion = $conexion->historico;
+            $coleccion = $conexion->atraso;
             $datos = $coleccion->find();
             return $datos;
         } catch (\Throwable $th) {
@@ -12,11 +12,70 @@ class Crud extends Conexion {
         }
     }
 
+    public function mostrarDatosnaves(){
+        try {
+            $conexion = parent::conectar();
+            $coleccion = $conexion->naves;
+            $datos = $coleccion->find();
+            return $datos;
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+    }
+
+    public function mostrarDatosgeneral(){
+        try {
+            $conexion = parent::conectar();
+            $coleccion = $conexion->general;
+            $datos = $coleccion->find();
+            return $datos;
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+    }
+
+    public function mostrarDatosco1(){
+        try {
+            $conexion = parent::conectar();
+            $coleccion = $conexion->cobranza1;
+            $datos = $coleccion->find();
+            return $datos;
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+    }
+
+    public function mostrarDatosco(){
+        try {
+            $conexion = parent::conectar();
+            $coleccion = $conexion->cobranza;
+            $datos = $coleccion->find();
+            return $datos;
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+    }
+
+    public function obtenerDocumento($id){
+        try {
+            $conexion = parent::conectar();
+            $coleccion = $conexion->atraso;
+            $datos = $coleccion -> findOne(
+                array(
+                    '_id' => new \MongoDB\BSON\ObjectId($id)
+                )
+            );
+            return $datos;
+        } catch (\Throwable $th) {
+            return $th -> getMessage();
+        }
+    }
+
     public function insertarDatos($datos){
         try {
             $conexion = parent::conectar();
-            $coleccion = $conexion->historico;
-            $respuesta = $coleccion->isertOne($datos);
+            $coleccion = $conexion->atraso;
+            $respuesta = $coleccion->insertOne($datos);
             return $respuesta;
         } catch (\Throwable $th) {
             return $th->getMessage();
